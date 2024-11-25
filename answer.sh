f28d3c1289904a694264096bcd76a23b01ce8951
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2024-11-25 13:46:08 +0900

    解答方法の修正

--- answer.sh
+++ answer.sh
@@ -3,17 +3,19 @@
 # ・$(ダラー, ドルマーク)とコマンドの間は、必ず半角スペースを入れてください。
 
 # 1. ターミナル/Gitbashを開いてください。(ここはコマンド実行は不要です。)
-# 2. ホームディレクトリに移動してください。(すでにホームディレクトリにいてもコマンドを実行して下さい)
 
+# 2. ホームディレクトリに移動してください。(すでにホームディレクトリにいてもコマンドを実行して下さい)
+$ cd
 
 # 3. カレントディレクトリから`~/geekationWorkspace`ディレクトリまで移動してください。(すでにオリエンテーションで作成済み)
-
+$ cd ~/geekationWorkspace
 
 # 4. `LinuxCommandPractice`というディレクトリを作成して、`LinuxCommandPractice`に移動してください。
-
+$ mkdir LinuxCommandPractice
+$ cd LinuxCommandPractice
 
 # 5. カレントディレクトリのパスを表示してください。
-
+$ pwd
 
 # 6. 次に、下の画像のようにGithubで`LinuxCommandPractice`という名前のリポジトリを作成してください。
 # 以下のGithubリポジトリ作成時の初期コマンドは書き換えなくてOKです。7に進んでください。
@@ -26,174 +28,57 @@ $ git branch -M main
 $ git remote add origin git@github.com:{Githubアカウント名}/LinuxCommandPractice.git
 $ git push -u origin main
 # ここまでGithubリポジトリ作成時の初期コマンド
+$ echo "# LinuxCommandPractice" >> README.md
+$ git init
+$ git add README.md
+$ git commit -m "first commit"
+$ git branch -M main
+$ git remote add origin https://github.com/Nakahata1021/LinuxCommandPractice.git
+$ git push -u origin main
 
 # 7. `LinuxCommandPractice`ディレクトリに`.git`ディレクトリが表示されていることを確認してください。
-
+$ $ ls -a
 
 # 8. `LinuxCommandPractice`ディレクトリ内に`practice_dir_1`ディレクトリを作成してください。
-
-
-# 9. `LinuxCommandPractice`ディレクトリ内に`practice_dir_1`ディレクトリが作成されていることを確認(表示)してください。
-
-
-# 10. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`というディレクトリを作成してください。**但し、相対パスを使用してください。**
-
-
-# 11. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`ディレクトリが作成されていることを確認(表示)してください。**但し、相対パスを使用してください。**
-
-
-# 12. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`./practice_dir_1/practice_dir_2`ディレクトリ内に`practice_file1.txt`、`practice_file2.txt`というファイルを**一回のコマンドで**作成してください。**但し、相対パスを使用してください。**
-
-
-# 13 `practice_dir_2`に移動してください。**但し、相対パスを使用してください。**
-
-
-# 14. `practice_file1.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**移動**してください。**但し、相対パスを使用してください。**
-
-
-# 15. `practice_file2.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**コピー**してください。**但し、相対パスを使用してください。**
-
-
-# 16. カレントディレクトリの`practice_file2.txt`を削除してください。**但し、相対パスを使用してください。**
-
-
-# 17. `practice_file2.txt`が削除されていることを確認(表示)してください。
-
-
-# 18. 2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ移動してください。**但し、相対パスを使用してください。**
-
-
-# 19. `practice_dir_2`ディレクトリを削除してください。**但し、相対パスを使用してください。**
-
-
-# 20. `vi`または`vim`を用いて、カレントディレクトリにある`practice_file1.txt`に`I'm good at Linux commands!`と書き込んでください。
-
-
-# 21. `practice_file1.txt`に書き込んだ内容を出力してください。
-
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 /
-$ cd
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~ (master)
-$ cd ~/geekationWorkspace
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace (master)
-$ mkdir LinuxCommandPractice
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace (master)
-$ cd LinuxCommandPractice
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ pwd
-/c/Users/nakahata jun/geekationWorkspace/LinuxCommandPractice
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ echo "# LinuxCommandPractice" >> README.md
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ git init
-Initialized empty Git repository in C:/Users/nakahata jun/geekationworkspace/LinuxCommandPractice/.git/
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ git add README.md
-warning: in the working copy of 'README.md', LF will be replaced by CRLF the next time Git touches it
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ git commit -m "first commit"
-[master (root-commit) 2881378] first commit
- 1 file changed, 1 insertion(+)
- create mode 100644 README.md
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (master)
-$ git branch -M main
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (main)
-$ git remote add origin https://github.com/Nakahata1021/LinuxCommandPractice.git
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (main)
-$ git push -u origin main
-Enumerating objects: 3, done.
-Counting objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 246 bytes | 246.00 KiB/s, done.
-Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
-To https://github.com/Nakahata1021/LinuxCommandPractice.git
- * [new branch]      main -> main
-branch 'main' set up to track 'origin/main'.
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (main)
-$ git checkout -b linux-command-practice
-Switched to a new branch 'linux-command-practice'
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ git branch
-* linux-command-practice
-  main
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ git push -u origin linux-command-practice
-Total 0 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
-remote:
-remote: Create a pull request for 'linux-command-practice' on GitHub by visiting:
-remote:      https://github.com/Nakahata1021/LinuxCommandPractice/pull/new/linux-command-practice
-remote:
-To https://github.com/Nakahata1021/LinuxCommandPractice.git
- * [new branch]      linux-command-practice -> linux-command-practice
-branch 'linux-command-practice' set up to track 'origin/linux-command-practice'.
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ ls -a
-./  ../  .git/  README.md
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ mkdir LinuxCommandPractice/practice_dir_1
-mkdir: cannot create directory ‘LinuxCommandPractice/practice_dir_1’: No such file or directory
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
 $ mkdir practice_dir_1
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 9. `LinuxCommandPractice`ディレクトリ内に`practice_dir_1`ディレクトリが作成されていることを確認(表示)してください。
 $ ls
-README.md  practice_dir_1/
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ mkdir LinuxCommandPractice/practice_dir_1/practice_dir_2
-mkdir: cannot create directory ‘LinuxCommandPractice/practice_dir_1/practice_dir_2’: No such file or directory
-
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 10. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`というディレクトリを作成してください。**但し、相対パスを使用してください。**
 $ mkdir practice_dir_1/practice_dir_2
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
-$ touch practice_dir_1/practice_dir_2/practice_file1.txt LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file2.txt
-touch: cannot touch 'LinuxCommandPractice/practice_dir_1/practice_dir_2/practice_file2.txt': No such file or directory
+# 11. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`ディレクトリが作成されていることを確認(表示)してください。**但し、相対パスを使用してください。**
+$ ls practice_dir_1/
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 12. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`./practice_dir_1/practice_dir_2`ディレクトリ内に`practice_file1.txt`、`practice_file2.txt`というファイルを**一回のコマンドで**作成してください。**但し、相対パスを使用してください。**
 $ touch practice_dir_1/practice_dir_2/practice_file1.txt practice_dir_1/practice_dir_2/practice_file2.txt
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 13 `practice_dir_2`に移動してください。**但し、相対パスを使用してください。**
 $ cd practice_dir_1/practice_dir_2
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2 (linux-command-practice)
+# 14. `practice_file1.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**移動**してください。**但し、相対パスを使用してください。**
 $ mv practice_file1.txt ../../
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2 (linux-command-practice)
+# 15. `practice_file2.txt`を2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ**コピー**してください。**但し、相対パスを使用してください。**
 $ cp practice_file2.txt ../../
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2 (linux-command-practice)
+# 16. カレントディレクトリの`practice_file2.txt`を削除してください。**但し、相対パスを使用してください。**
 $ rm practice_file2.txt
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2 (linux-command-practice)
+# 17. `practice_file2.txt`が削除されていることを確認(表示)してください。
 $ ls
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice/practice_dir_1/practice_dir_2 (linux-command-practice)
+# 18. 2つ上の階層のディレクトリ(`LinuxCommandPractice`)へ移動してください。**但し、相対パスを使用してください。**
 $ cd ../../
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 19. `practice_dir_2`ディレクトリを削除してください。**但し、相対パスを使用してください。**
 $ rm -r practice_dir_1/practice_dir_2
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 20. `vi`または`vim`を用いて、カレントディレクトリにある`practice_file1.txt`に`I'm good at Linux commands!`と書き込んでください。
 $ vi practice_file1.txt
 
-nakahata jun@DESKTOP-BN3A9G8 MINGW64 ~/geekationWorkspace/LinuxCommandPractice (linux-command-practice)
+# 21. `practice_file1.txt`に書き込んだ内容を出力してください。
 $ cat practice_file1.txt
 I'm good at Linux commands!
+
